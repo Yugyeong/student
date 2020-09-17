de1dbf355271492b8d90e931843519c6a21865d4
root <root@root>
2020-09-17 12:12:50 +0900

    j

--- src/main/java/myProject/external/MyClassService.java
+++ src/main/java/myProject/external/MyClassService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="myclassmanagement", url="${api.url.myclassmanagement}")
+@FeignClient(name="myClassManagement", url="${api.url.myClassManagement}")
 public interface MyClassService {
     
     @RequestMapping(method=RequestMethod.POST, path="/myClasses/sc")
